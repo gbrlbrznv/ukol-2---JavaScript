c5a6b1438f05838c0b120a4bfa46486d912916c5
Gabriela Březinová <gabri.brezinova@gmail.com>
2024-02-21 12:50:16 +0100

    commit message

---
+++ script.js
@@ -0,0 +1,12 @@
+const emailPrijmeni = prompt("Zadej příjmení bez diakritiky").toLocaleLowerCase()
+const emailJmeno = prompt ("Zadej jméno bez diakritiky").toLocaleLowerCase()
+
+const prijmeniTrim = emailPrijmeni.trim()
+
+const prijmeniSlice = prijmeniTrim.slice(0, 5)
+
+const jmenoTrim = emailJmeno.trim()
+
+const jmenoSlice = jmenoTrim.slice(0, 3)
+
+document.body.innerHTML = prijmeniSlice + jmenoSlice + "@fit.cvut.cz"
